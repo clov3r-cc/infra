9d4f4ea74642f1309e53dafdefa4b48688723d7d
Lucky3028 <yubi@clov3r.cc>
2025-01-13 02:34:12 +0900

    fmt: run fmt

--- terraform/cloudflare_zero_trust.tf
+++ terraform/cloudflare_zero_trust.tf
@@ -48,7 +48,7 @@ resource "cloudflare_zero_trust_access_application" "pxmx01-mng" {
 
 resource "cloudflare_zero_trust_access_group" "allow_github" {
   account_id = local.cloudflare_account_id
-  name = "Allow GitHub"
+  name       = "Allow GitHub"
   include {
     # GitHub
     login_method = ["3b628f5e-ce37-44d3-9182-ab59c1331f53"]
