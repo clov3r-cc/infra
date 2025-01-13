05574ebd4150bbbe784de3d3038b0dfd43be50e4
Lucky3028 <yubi@clov3r.cc>
2025-01-13 13:23:22 +0900

    refactor: move provider defs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,3 @@
-terraform {
-  cloud {
-    organization = "clov3r-cc"
-
-    workspaces {
-      name = "infra"
-    }
-  }
-
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
-
-provider "random" {
-}
-
 locals {
   # NOTE: account_id は非シークレット
   # https://github.com/cloudflare/wrangler-legacy/issues/209#issuecomment-541654484
