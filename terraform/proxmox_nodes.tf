e78014e69c5afc2c010acae6bc1cfe0b9d659228
Lucky3028 <yubi@clov3r.cc>
2025-01-31 21:37:07 +0900

    feat: add VMs

--- terraform/proxmox_nodes.tf
+++ terraform/proxmox_nodes.tf
@@ -30,15 +30,43 @@ module "k8s_worker_nodes__prod" {
 
   env_name = "prod"
 
-  worker_allocated_host = {
-    1 = local.pve_hosts["pve-01"]["host_name"]
-  }
   vm_user            = var.vm_user
   vm_user_password   = random_password.vm_user_password.result
   vm_ssh_public_key  = var.vm_ssh_public_key
   vm_template        = local.vm_template
   vm_os_disk_storage = local.vm_os_disk_storage
 
+  gateway_allocated_host            = local.pve_hosts["pve-01"]["host_name"]
+  gateway_vm_id                     = 801
+  gateway_memory                    = 2048
+  gateway_sockets                   = 1
+  gateway_cores                     = 2
+  gateway_public-net_bridge         = local.vm_public-net_bridge
+  gateway_public-net_subnet_cidr    = local.vm_public-net_subnet_cidr
+  gateway_internal-net_bridge       = local.vm_internal-net_bridge
+  gateway_internal-net_subnet_cidr  = local.vm_internal-net_subnet_cidr
+  gateway_internal-net_host-section = 2
+  gateway_os_disk_size              = 20
+
+  control_plane_allocated_host = {
+    1 = local.pve_hosts["pve-01"]["host_name"]
+    2 = local.pve_hosts["pve-01"]["host_name"]
+    3 = local.pve_hosts["pve-01"]["host_name"]
+  }
+  control_plane_first_vm_id                     = 811
+  control_plane_memory                          = 1024 * 3
+  control_plane_sockets                         = 1
+  control_plane_cores                           = 3
+  control_plane_internal-net_bridge             = local.vm_internal-net_bridge
+  control_plane_internal-net_subnet_cidr        = local.vm_internal-net_subnet_cidr
+  control_plane_first_internal-net_host-section = 11
+  control_plane_os_disk_size                    = 20
+
+  worker_allocated_host = {
+    1 = local.pve_hosts["pve-01"]["host_name"]
+    2 = local.pve_hosts["pve-01"]["host_name"]
+    3 = local.pve_hosts["pve-01"]["host_name"]
+  }
   worker_first_vm_id                     = 821
   worker_memory                          = 1024 * 6
   worker_sockets                         = 1
