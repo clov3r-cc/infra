0e7cd60b353e2df08ea08abd9e82daa3e5c4aae2
Lucky3028 <yubi@clov3r.cc>
2025-02-03 22:14:06 +0900

    fix: access app domain contains domain

--- terraform/pve_01_console_access.tf
+++ terraform/pve_01_console_access.tf
@@ -12,7 +12,7 @@ resource "cloudflare_zero_trust_access_application" "pxmx01-mng" {
   zone_id          = local.cloudflare_zone_id
   type             = "self_hosted"
   name             = "Access application for ${cloudflare_dns_record.pxmx01-mng.name}"
-  domain           = cloudflare_dns_record.pxmx01-mng.name
+  domain           = "${cloudflare_dns_record.pxmx01-mng.name}.${local.domain}"
   session_duration = "24h"
   policies = [{
     id         = cloudflare_zero_trust_access_policy.allow_github.id
