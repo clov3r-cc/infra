cb7a50deee675a715f3620c2ba2169c19c682fbb
Lucky3028 <yubi@clov3r.cc>
2025-02-03 20:57:25 +0900

    fix: fix syntax changes

--- terraform/cloudflare_zero_trust.tf
+++ terraform/cloudflare_zero_trust.tf
@@ -1,8 +1,8 @@
 resource "cloudflare_zero_trust_access_group" "allow_github" {
-  account_id = local.cloudflare_account_id
-  name       = "Allow GitHub"
-  include {
+  zone_id = local.cloudflare_zone_id
+  name    = "Allow GitHub"
+  include = [{
     # GitHub
     login_method = ["3b628f5e-ce37-44d3-9182-ab59c1331f53"]
-  }
+  }]
 }
--- terraform/pve_01_cloudflared.tf
+++ terraform/pve_01_cloudflared.tf
@@ -3,24 +3,26 @@ resource "random_password" "tunnel_secret" {
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared" "cloudflared-01" {
-  account_id = local.cloudflare_account_id
-  name       = "Terraform tunnel to proxmox-01 container in lucky-proxmox-01"
-  secret     = base64sha256(random_password.tunnel_secret.result)
+  account_id    = local.cloudflare_account_id
+  name          = "Terraform tunnel to proxmox-01 container in lucky-proxmox-01"
+  tunnel_secret = base64sha256(random_password.tunnel_secret.result)
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "cloudflared-01" {
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.cloudflared-01.id
   account_id = local.cloudflare_account_id
-  config {
-    ingress_rule {
-      hostname = cloudflare_record.pxmx01-mng.hostname
-      service  = "https://192.168.20.2:8006"
-      origin_request {
-        no_tls_verify = true
+  config = {
+    ingress = [
+      {
+        hostname = cloudflare_dns_record.pxmx01-mng.name
+        service  = "https://192.168.20.2:8006"
+        origin_request = {
+          no_tls_verify = true
+        }
+      },
+      {
+        service = "http_status:404"
       }
-    }
-    ingress_rule {
-      service = "http_status:404"
-    }
+    ]
   }
 }
