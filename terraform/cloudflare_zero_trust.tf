a87ea03055507a2b68923052088b4980cbf8d1c2
Lucky3028 <yubi@clov3r.cc>
2025-02-03 21:08:39 +0900

    refactor: move common settings for Cloudflared Auth

--- terraform/cloudflare_zero_trust.tf
+++ terraform/cloudflare_zero_trust.tf
@@ -6,3 +6,32 @@ resource "cloudflare_zero_trust_access_group" "allow_github" {
     login_method = ["3b628f5e-ce37-44d3-9182-ab59c1331f53"]
   }]
 }
+
+resource "cloudflare_zero_trust_access_policy" "allow_github" {
+  account_id = local.cloudflare_account_id
+  name       = "To SSO with GitHub"
+  decision   = "allow"
+  include = [{
+    group = {
+      id = cloudflare_zero_trust_access_group.allow_github.id
+    }
+  }]
+}
+
+resource "cloudflare_zero_trust_access_service_token" "this" {
+  name     = "managed token"
+  zone_id  = local.cloudflare_zone_id
+  duration = "${24 * 365}h" # 1y
+}
+
+# Service Token での認証はアクション（`decision`）を`Service Auth`（`non_identity`）にする必要がある
+resource "cloudflare_zero_trust_access_policy" "allow_service_token" {
+  account_id = local.cloudflare_account_id
+  name       = "Allow bypass by service token"
+  decision   = "non_identity"
+  include = [{
+    service_token = {
+      token_id = cloudflare_zero_trust_access_service_token.this.id
+    }
+  }]
+}
--- terraform/pve_01_console_access.tf
+++ terraform/pve_01_console_access.tf
@@ -21,32 +21,3 @@ resource "cloudflare_zero_trust_access_application" "pxmx01-mng" {
     precedence = "2"
   }]
 }
-
-resource "cloudflare_zero_trust_access_policy" "allow_github" {
-  account_id = local.cloudflare_account_id
-  name       = "To SSO with GitHub"
-  decision   = "allow"
-  include = [{
-    group = {
-      id = cloudflare_zero_trust_access_group.allow_github.id
-    }
-  }]
-}
-
-resource "cloudflare_zero_trust_access_service_token" "this" {
-  name     = "managed token"
-  zone_id  = local.cloudflare_zone_id
-  duration = "${24 * 365}h" # 1y
-}
-
-# Service Token での認証はアクション（`decision`）を`Service Auth`（`non_identity`）にする必要がある
-resource "cloudflare_zero_trust_access_policy" "allow_service_token" {
-  account_id = local.cloudflare_account_id
-  name       = "Allow bypass by service token"
-  decision   = "non_identity"
-  include = [{
-    service_token = {
-      token_id = cloudflare_zero_trust_access_service_token.this.id
-    }
-  }]
-}
