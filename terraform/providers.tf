4ec103a597a3aae89e5245c18ce798fd475f8bfe
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-10-15 23:28:59 +0900

    chore: update telmate/proxmox

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -20,7 +20,7 @@ terraform {
     }
     proxmox = {
       source  = "telmate/proxmox"
-      version = "3.0.2-rc4"
+      version = "3.0.2-rc04"
     }
     null = {
       source  = "hashicorp/null"
