2dc9da411e789faa00ae3e81a9c6d372cc17167d
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-10-16 00:07:51 +0900

    fix!: migrate

--- terraform/cloudflare_zero_trust.tf
+++ terraform/cloudflare_zero_trust.tf
@@ -1,8 +1,8 @@
 resource "cloudflare_zero_trust_access_group" "allow_github" {
   account_id = local.cloudflare_account_id
   name       = "Allow GitHub"
-  include {
+  include = [{
     # GitHub
     login_method = ["3b628f5e-ce37-44d3-9182-ab59c1331f53"]
-  }
+  }]
 }
