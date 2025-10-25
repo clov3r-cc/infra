cfc2d8474019e8354879ba9445426c683d277bbb
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-10-25 19:32:43 +0900

    feat: add cloudflared

--- terraform/pve_01_cloudflared.tf
+++ terraform/pve_01_cloudflared.tf
@@ -1,28 +1,72 @@
-# TODO: Fix me
+resource "random_password" "tunnel_secret__cloudflared-01" {
+  length = 64
+}
 
-# resource "random_password" "tunnel_secret" {
-#   length = 64
-# }
+resource "cloudflare_zero_trust_tunnel_cloudflared" "cloudflared-01" {
+  account_id = data.cloudflare_account.me.account_id
+  name       = "Terraform tunnel for proxmox-01 container in proxmox-01 (This resource is managed with Terraform)"
+  secret     = base64sha256(random_password.tunnel_secret__cloudflared-01.result)
+}
 
-# resource "cloudflare_zero_trust_tunnel_cloudflared" "cloudflared-01" {
-#   account_id = data.cloudflare_account.me.account_id
-#   name       = "Terraform tunnel to proxmox-01 container in lucky-proxmox-01"
-#   secret     = base64sha256(random_password.tunnel_secret.result)
-# }
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "cloudflared-01" {
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.cloudflared-01.id
+  account_id = data.cloudflare_account.me.account_id
+  config {
+    ingress_rule {
+      hostname = cloudflare_dns_record.pxmx01-mng.hostname
+      service  = "https://192.168.20.2:8006"
+      origin_request {
+        no_tls_verify = true
+      }
+    }
+    ingress_rule {
+      service = "http_status:404"
+    }
+  }
+}
 
-# resource "cloudflare_zero_trust_tunnel_cloudflared_config" "cloudflared-01" {
-#   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.cloudflared-01.id
-#   account_id = data.cloudflare_account.me.account_id
-#   config {
-#     ingress_rule {
-#       hostname = cloudflare_record.pxmx01-mng.hostname
-#       service  = "https://192.168.20.2:8006"
-#       origin_request {
-#         no_tls_verify = true
-#       }
-#     }
-#     ingress_rule {
-#       service = "http_status:404"
-#     }
-#   }
-# }
+resource "cloudflare_dns_record" "pxmx01-mng" {
+  zone_id = data.cloudflare_zone.clov3r-cc.zone_id
+  name    = "pxmx01-mng"
+  content = cloudflare_zero_trust_tunnel_cloudflared.cloudflared-01.cname
+  type    = "CNAME"
+  proxied = true
+  ttl     = 1 # Auto
+  comment = "for Cloudflare Tunnel with Proxmox#1 (This resource is managed with Terraform)"
+}
+
+resource "cloudflare_zero_trust_access_application" "pxmx01-mng" {
+  zone_id          = data.cloudflare_zone.clov3r-cc.zone_id
+  name             = "Access application for ${cloudflare_dns_record.pxmx01-mng.hostname} (This resource is managed with Terraform)"
+  domain           = cloudflare_dns_record.pxmx01-mng.hostname
+  session_duration = "24h"
+}
+
+resource "cloudflare_zero_trust_access_policy" "pxmx01-mng" {
+  application_id = cloudflare_zero_trust_access_application.pxmx01-mng.id
+  zone_id        = data.cloudflare_zone.clov3r-cc.zone_id
+  name           = "Web Login Policy for ${cloudflare_dns_record.pxmx01-mng.hostname}"
+  precedence     = "1"
+  decision       = "allow"
+  include {
+    group = [cloudflare_zero_trust_access_group.allow_github.id]
+  }
+}
+
+resource "cloudflare_zero_trust_access_service_token" "managed" {
+  name     = "managed token"
+  zone_id  = data.cloudflare_zone.clov3r-cc.zone_id
+  duration = "8760h" # 1year
+}
+
+# Service Token での認証はアクション（`decision`）を`Service Auth`（`non_identity`）にする必要がある
+resource "cloudflare_zero_trust_access_policy" "pxmx01-mng__srv-token" {
+  application_id = cloudflare_zero_trust_access_application.pxmx01-mng.id
+  zone_id        = data.cloudflare_zone.clov3r-cc.zone_id
+  name           = "CLI Policy for ${cloudflare_dns_record.pxmx01-mng.hostname}"
+  precedence     = "2"
+  decision       = "non_identity"
+  include {
+    service_token = [cloudflare_zero_trust_access_service_token.managed.id]
+  }
+}
