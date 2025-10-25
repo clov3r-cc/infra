de7e0793965f34ba6ffca4abd95cf70b36ef41ef
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-10-25 18:15:12 +0900

    fix: update compatibility_date

--- terraform/my_homepage.tf
+++ terraform/my_homepage.tf
@@ -10,7 +10,7 @@ resource "cloudflare_pages_project" "homepage" {
     }
     production = {
       always_use_latest_compatibility_date = false # The `always_use_latest_compatibility_date` property cannot be true for Production deployments
-      compatibility_date                   = "2024-06-03"
+      compatibility_date                   = "2025-09-15"
       fail_open                            = true       # Bypass Cloudflare Pages Functions when requests to Functions count towards your quota (default value in GUI)
       usage_model                          = "standard" # In free plan, we can select only standard model (default value in GUI)
     }
