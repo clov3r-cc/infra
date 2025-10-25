3711a3e2cc6c754fb4809ef07e5d87b9c631bfea
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-10-25 19:54:08 +0900

    fix: fix syntax

--- terraform/pve_01_cloudflared.tf
+++ terraform/pve_01_cloudflared.tf
@@ -36,18 +36,19 @@ resource "cloudflare_zero_trust_access_application" "pxmx01-mng" {
   zone_id          = data.cloudflare_zone.clov3r-cc.zone_id
   name             = "Access application for ${cloudflare_dns_record.pxmx01-mng.name}.${data.cloudflare_zone.clov3r-cc.name} (This resource is managed with Terraform)"
   domain           = "${cloudflare_dns_record.pxmx01-mng.name}.${data.cloudflare_zone.clov3r-cc.name}"
+  type             = "self_hosted"
   session_duration = "24h"
 }
 
 resource "cloudflare_zero_trust_access_policy" "pxmx01-mng" {
-  application_id = cloudflare_zero_trust_access_application.pxmx01-mng.id
-  zone_id        = data.cloudflare_zone.clov3r-cc.zone_id
-  name           = "Web Login Policy for ${cloudflare_dns_record.pxmx01-mng.name}.${data.cloudflare_zone.clov3r-cc.name}"
-  precedence     = "1"
-  decision       = "allow"
-  include {
-    group = [cloudflare_zero_trust_access_group.allow_github.id]
-  }
+  account_id = data.cloudflare_account.me.account_id
+  name       = "Web Login Policy for ${cloudflare_dns_record.pxmx01-mng.name}.${data.cloudflare_zone.clov3r-cc.name}"
+  decision   = "allow"
+  include = [{
+    group = [{
+      id = cloudflare_zero_trust_access_group.allow_github.id
+    }]
+  }]
 }
 
 resource "cloudflare_zero_trust_access_service_token" "managed" {
@@ -58,12 +59,12 @@ resource "cloudflare_zero_trust_access_service_token" "managed" {
 
 # Service Token での認証はアクション（`decision`）を`Service Auth`（`non_identity`）にする必要がある
 resource "cloudflare_zero_trust_access_policy" "pxmx01-mng__srv-token" {
-  application_id = cloudflare_zero_trust_access_application.pxmx01-mng.id
-  zone_id        = data.cloudflare_zone.clov3r-cc.zone_id
-  name           = "CLI Policy for ${cloudflare_dns_record.pxmx01-mng.name}.${data.cloudflare_zone.clov3r-cc.name}"
-  precedence     = "2"
-  decision       = "non_identity"
-  include {
-    service_token = [cloudflare_zero_trust_access_service_token.managed.id]
-  }
+  account_id = data.cloudflare_account.me.account_id
+  name       = "CLI Policy for ${cloudflare_dns_record.pxmx01-mng.name}.${data.cloudflare_zone.clov3r-cc.name}"
+  decision   = "non_identity"
+  include = [{
+    service_token = {
+      token_id = cloudflare_zero_trust_access_service_token.managed.id
+    }
+  }]
 }
