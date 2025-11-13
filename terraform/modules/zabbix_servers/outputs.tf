904c816440f05534afbbdf45e08f3ce51450dc95
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-11-14 00:55:26 +0900

    feat: add outputs

--- terraform/modules/zabbix_servers/outputs.tf
+++ terraform/modules/zabbix_servers/outputs.tf
@@ -1,6 +1,18 @@
 output "zabbix_servers" {
   value = [for vm in proxmox_vm_qemu.server : {
-    vm_name = vm.name
-    ip0     = vm.ssh_host
+    host_name   = vm.current_node
+    id          = vm.vmid
+    name        = vm.name
+    description = vm.description
+    ethernet_adapters = {
+      ip0 = {
+        bridge = vm.network[0].bridge
+        config = vm.ipconfig0
+      }
+      ip1 = {
+        bridge = vm.network[1].bridge
+        config = vm.ipconfig1
+      }
+    }
   }]
 }
