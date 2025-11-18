f559ac89e0f00f677c53c78d40d7a588366a6eaf
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-11-19 02:29:52 +0900

    feat: add prd-ans

--- terraform/prd/outputs.tf
+++ terraform/prd/outputs.tf
@@ -15,6 +15,23 @@ output "zabbix_servers__prd" {
   }]
 }
 
+output "ansible_players__prd" {
+  value = [for vm in proxmox_vm_qemu.ansible_player : {
+    host_name   = vm.current_node
+    id          = vm.vmid
+    name        = vm.name
+    description = vm.description
+    ethernet_adapters = {
+      ip0 = merge(
+        { bridge = vm.network[0].bridge },
+        # (?P<name>x):	named capture group, named name, for sub-pattern x
+        # https://developer.hashicorp.com/terraform/language/functions/regex
+        regex("ip=(?P<ip_address>.*),gw=(?P<default_gateway>.*)", vm.ipconfig0)
+      )
+    }
+  }]
+}
+
 # TODO: Fix me
 # output "prod__k8s_gateway_node" {
 #   value = module.prod__k8s_nodes.gateway
