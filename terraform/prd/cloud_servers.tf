9c9ef8dcbfcfd79deb60805d3c24e64a87a25b52
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-11-20 19:41:08 +0900

    feat: add VCN

---
+++ terraform/prd/cloud_servers.tf
@@ -0,0 +1,8 @@
+resource "oci_core_vcn" "my_vcn" {
+  compartment_id                   = local.oracle_cloud_tenancy_id
+  cidr_blocks                      = "10.0.0.0/16"
+  display_name                     = "my_vcn"
+  dns_label                        = "my_vcn"
+  is_ipv6enabled                   = false
+  is_oracle_gua_allocation_enabled = false
+}
