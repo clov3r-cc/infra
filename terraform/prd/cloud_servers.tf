b64121bcf79f3f2f005abddcfb8e684b51125413
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-11-21 00:20:23 +0900

    fix: use id not object

--- terraform/prd/cloud_servers.tf
+++ terraform/prd/cloud_servers.tf
@@ -17,7 +17,7 @@ resource "oci_core_internet_gateway" "my_vcn_internet_gateway" {
 resource "oci_core_subnet" "my_vcn_subnet" {
   compartment_id      = local.oracle_cloud_tenancy_id
   vcn_id              = oci_core_vcn.my_vcn.id
-  availability_domain = data.oci_identity_availability_domains.ad
+  availability_domain = data.oci_identity_availability_domains.ad.compartment_id
   cidr_block          = "10.0.21.0/24"
   display_name        = "my_vcn_subnet__prod"
   dns_label           = "prod"
