22ce0903fda6201c0d39c5abb6879db9d6179560
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-11-20 19:47:46 +0900

    fix: fix dns label

--- terraform/prd/cloud_servers.tf
+++ terraform/prd/cloud_servers.tf
@@ -2,7 +2,7 @@ resource "oci_core_vcn" "my_vcn" {
   compartment_id                   = local.oracle_cloud_tenancy_id
   cidr_blocks                      = "10.0.0.0/16"
   display_name                     = "my_vcn"
-  dns_label                        = "my_vcn"
+  dns_label                        = "clov3r"
   is_ipv6enabled                   = false
   is_oracle_gua_allocation_enabled = false
 }
