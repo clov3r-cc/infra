914b0dc0a8214d7ad7452fdcb66891a4d96f7cd1
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-11-22 20:41:22 +0900

    feat: add public IP

--- terraform/prd/cloud_servers.tf
+++ terraform/prd/cloud_servers.tf
@@ -103,7 +103,7 @@ resource "oci_core_instance" "cloud_server" {
 
   create_vnic_details {
     subnet_id                 = oci_core_subnet.my_vcn_subnet.id
-    assign_public_ip          = false
+    assign_public_ip          = true
     assign_private_dns_record = false
     nsg_ids                   = [oci_core_network_security_group.my_vcn_nw_sg.id]
   }
