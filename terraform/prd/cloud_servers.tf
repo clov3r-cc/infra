a22bb8650e24ec5d7f525eb585ede6b048bc1bd9
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-11-22 17:37:37 +0900

    fix: set resource name with naming rules

--- terraform/prd/cloud_servers.tf
+++ terraform/prd/cloud_servers.tf
@@ -1,8 +1,7 @@
 resource "oci_core_vcn" "my_vcn" {
   compartment_id = local.oracle_cloud_tenancy_id
   cidr_blocks    = ["10.0.21.0/24"]
-  display_name   = "my_vcn__prod"
-  dns_label      = "clov3r"
+  display_name   = "prd-vcn-01"
   is_ipv6enabled = false
 }
 
@@ -10,21 +9,20 @@ resource "oci_core_internet_gateway" "my_vcn_internet_gateway" {
   compartment_id = local.oracle_cloud_tenancy_id
   vcn_id         = oci_core_vcn.my_vcn.id
   enabled        = true
-  display_name   = "my_vcn_internet_gateway__prod"
+  display_name   = "prd-igw-01"
 }
 
 resource "oci_core_subnet" "my_vcn_subnet" {
   compartment_id = local.oracle_cloud_tenancy_id
   vcn_id         = oci_core_vcn.my_vcn.id
   cidr_block     = "10.0.21.0/24"
-  display_name   = "my_vcn_subnet__prod"
-  dns_label      = "prod"
+  display_name   = "prd-sbn-01"
 }
 
 resource "oci_core_route_table" "my_vcn_route_table" {
   compartment_id = local.oracle_cloud_tenancy_id
   vcn_id         = oci_core_vcn.my_vcn.id
-  display_name   = "my_vcn_route_table__prod"
+  display_name   = "prd-rtb-01"
 
   route_rules {
     destination       = "0.0.0.0/0"
@@ -38,7 +36,7 @@ resource "oci_core_route_table" "my_vcn_route_table" {
 resource "oci_core_network_security_group" "my_vcn_nw_sg" {
   compartment_id = local.oracle_cloud_tenancy_id
   vcn_id         = oci_core_vcn.my_vcn.id
-  display_name   = "my_vcn_nw_sg__prod"
+  display_name   = "prd-nsg-01"
 }
 
 resource "oci_core_network_security_group_security_rule" "my_vcn_nw_sg__egress__allow_all_traffics" {
