4fdb5a96fffc0e7405b5b767a7a2e04c0c3e0cbc
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-11-25 00:14:24 +0900

    feat: set python path for csv

--- terraform/prd/cloud_servers.tf
+++ terraform/prd/cloud_servers.tf
@@ -136,6 +136,7 @@ resource "ansible_group" "cloud_server" {
   variables = {
     ansible_user                 = local.machine_user
     ansible_ssh_private_key_file = local.ansible_ssh_private_key_path
+    ansible_python_interpreter   = "/usr/bin/python3"
   }
 }
 
