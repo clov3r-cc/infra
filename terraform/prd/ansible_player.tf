c2c541137996e3fdca3d8806144669cd7c8ff007
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2025-11-24 21:35:24 +0900

    fix: add dependency

--- terraform/prd/ansible_player.tf
+++ terraform/prd/ansible_player.tf
@@ -141,11 +141,11 @@ resource "proxmox_vm_qemu" "ansible_player" {
 }
 
 resource "terraform_data" "ssh_private_key__ansible_player" {
-  for_each = local.vm_settings__ansible_player
+  for_each = { for vm in proxmox_vm_qemu.ansible_player : vm.name => vm }
 
   connection {
     type        = "ssh"
-    host        = proxmox_vm_qemu.ansible_player[each.key].ssh_host
+    host        = each.value.ssh_host
     user        = local.machine_user
     private_key = base64decode(var.vm_ssh_private_key)
   }
@@ -184,19 +184,30 @@ resource "ansible_host" "ansible_player" {
   }
 }
 
-resource "terraform_data" "send_ansible_files" {
-  connection {
-    type        = "ssh"
-    host        = proxmox_vm_qemu.ansible_player[each.key].ssh_host
-    user        = local.machine_user
-    private_key = base64decode(var.vm_ssh_private_key)
-  }
+resource "terraform_data" "make_ansible_inventory" {
+  # TODO: Whenever the number of VM types changes, rewrite this section.
+  depends_on = [
+    ansible_host.ansible_player, ansible_host.cloud_server, ansible_host.zabbix_server
+  ]
+
   provisioner "local-exec" {
     command = <<EOF
     cd $(git rev-parse --show-toplevel)/terraform/${local.env}
     ansible-galaxy collection install cloud.terraform
     ansible-inventory --inventory ansible/inventory.yaml --list --yaml --output ansible/inventory.yaml
     EOF
+  }
+}
+
+resource "terraform_data" "send_ansible_files" {
+  for_each   = { for vm in proxmox_vm_qemu.ansible_player : vm.name => vm }
+  depends_on = [terraform_data.make_ansible_inventory]
+
+  connection {
+    type        = "ssh"
+    host        = each.value.ssh_host
+    user        = local.machine_user
+    private_key = base64decode(var.vm_ssh_private_key)
   }
   provisioner "remote-exec" {
     inline = ["mkdir ~/ansible || :"]
