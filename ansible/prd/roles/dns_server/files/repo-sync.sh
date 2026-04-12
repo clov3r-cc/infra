df5fbaad978977573c8970456bfb2f8ca5dad43f
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2026-04-13 00:15:05 +0900

    feat: use ELRepo and MariaDB dnf repo

--- ansible/prd/roles/dns_server/files/repo-sync.sh
+++ ansible/prd/roles/dns_server/files/repo-sync.sh
@@ -5,10 +5,24 @@ exec > >(awk '{ print strftime("%Y/%m/%d %H:%M:%S"), $0; fflush() }') 2>&1
 
 echo "=== repo-sync started ==="
 
-reposync --download-metadata --delete --newest-only --repoid=baseos --download-path=/var/www/html/repos/
-reposync --download-metadata --delete --newest-only --repoid=appstream --download-path=/var/www/html/repos/
-reposync --download-metadata --delete --newest-only --repoid=crb --download-path=/var/www/html/repos/
-reposync --download-metadata --delete --newest-only --repoid=extras --download-path=/var/www/html/repos/
-reposync --download-metadata --delete --newest-only --repoid='copr:copr.fedorainfracloud.org:noa:rust' --download-path=/var/www/html/repos/copr-noa-rust/
+DOWNLOAD_PATH="/var/www/html/repos"
+REPO_IDS=(
+  baseos
+  appstream
+  crb
+  extras
+  highavailability
+
+  # 追加
+  elrepo
+  mariadb
+  # COPRは配列に含めない
+)
+
+for repoid in "${REPO_IDS[@]}"; do
+  reposync --download-metadata --delete --newest-only --repoid="$repoid" --download-path="${DOWNLOAD_PATH}/"
+done
+# COPR はダウンロード先のディレクトリを明示的に指定するため個別に実行
+reposync --download-metadata --delete --newest-only --repoid='copr:copr.fedorainfracloud.org:noa:rust' --download-path="${DOWNLOAD_PATH}/copr-noa-rust/"
 
 echo "=== repo-sync finished ==="
