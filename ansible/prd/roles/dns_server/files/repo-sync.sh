5b1e44eddaf4793dc7ff4e1939681ee61cd5e2b9
Lucky3028 <24449974+Lucky3028@users.noreply.github.com>
2026-04-17 00:16:00 +0900

    feat: add dnf local mirror for ELRepo and MariaDB

--- ansible/prd/roles/dns_server/files/repo-sync.sh
+++ ansible/prd/roles/dns_server/files/repo-sync.sh
@@ -25,4 +25,15 @@ done
 # COPR はダウンロード先のディレクトリを明示的に指定するため個別に実行
 reposync --download-metadata --delete --newest-only --repoid='copr:copr.fedorainfracloud.org:noa:rust' --download-path="${DOWNLOAD_PATH}/copr-noa-rust/"
 
+KEY_PATH="${DOWNLOAD_PATH}/keys"
+mkdir -p "$KEY_PATH"
+GPG_KEYS=(
+  https://www.elrepo.org/RPM-GPG-KEY-v2-elrepo.org
+  https://ftp.yz.yamagata-u.ac.jp/pub/dbms/mariadb/yum/RPM-GPG-KEY-MariaDB
+)
+for key_url in "${GPG_KEYS[@]}"; do
+  echo "Downloading GPG key: $key_url"
+  curl -fsSL "$key_url" -o "${KEY_PATH}/${key_url##*/}"
+done
+
 echo "=== repo-sync finished ==="
